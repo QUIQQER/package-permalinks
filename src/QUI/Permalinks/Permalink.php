c0cdfaba73d2624590c102d667cac352462e120b
addius8 <addius8@web.de>
2021-03-25 09:37:53 +0100

    getting permalinks to work (main function of Nexgam)

--- src/QUI/Permalinks/Permalink.php
+++ src/QUI/Permalinks/Permalink.php
@@ -242,6 +242,7 @@ class Permalink
                 return;
             }
         } catch (QUI\Exception $Exception) {
+
         }
 
         try {
@@ -366,6 +367,7 @@ class Permalink
 
         try {
             $Project = $Rewrite->getProject();
+
             $Site    = self::getSiteByPermalink($Project, $url);
 
             if (\strpos($url, '.html') !== false
@@ -381,6 +383,22 @@ class Permalink
 
             $Rewrite->setSite($Site);
         } catch (QUI\Exception $Exception) {
+            QUI\System\Log::writeException($Exception);
         }
     }
+
+    /**
+     * @param \QUI\Projects\Site\Edit $Site
+     */
+    public static function onUrlRewritten ($Site, &$url) {
+
+        // try getting the permalink for siteId
+        try {
+            $permalinkForSite = self::getPermalinkForSite($Site);
+            $url = $permalinkForSite;
+        } catch (\Exception $Exception) {
+//            QUI\System\Log::writeException($Exception);
+        }
+    }
+
 }

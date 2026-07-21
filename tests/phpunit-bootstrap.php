3d88f75ccb4cd6a6818b2e5188ceaa423bb5cf7d
Henning <leutz@pcsg.de>
2026-07-21 11:09:27 +0200

    test: cover permalink lifecycle and events

--- tests/phpunit-bootstrap.php
+++ tests/phpunit-bootstrap.php
@@ -9,3 +9,21 @@ if (!defined('QUIQQER_AJAX')) {
 }
 
 require_once __DIR__ . '/../../../../bootstrap.php';
+
+spl_autoload_register(static function (string $class): void {
+    $prefix = 'QUI\\Permalinks\\';
+
+    if (!str_starts_with($class, $prefix)) {
+        return;
+    }
+
+    $file = dirname(__DIR__) . '/src/QUI/Permalinks/'
+        . str_replace('\\', '/', substr($class, strlen($prefix)))
+        . '.php';
+
+    if (is_file($file)) {
+        require_once $file;
+    }
+});
+
+QUI\System\TestCleanup::register();
